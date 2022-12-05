62d5523fb56d19e0d7e0aa577a6674a95dfb99ff
masu <kingmasu7@gmail.com>
2022-12-05 22:00:32 +0000

    6th commit

--- hotel/urls.py
+++ hotel/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from reserve import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('reserve.urls')),
 ]
 
